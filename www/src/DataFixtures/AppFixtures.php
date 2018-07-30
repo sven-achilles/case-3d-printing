24a632e10b4febe042baae8eecc946bc3b81e9bd
Sven van Zoelen <sven@achillescm.nl>
2018-07-30 22:17:13 +0200

    users and prints are now random created in the fixture file.

--- www/src/DataFixtures/AppFixtures.php
+++ www/src/DataFixtures/AppFixtures.php
@@ -13,6 +13,14 @@ class AppFixtures extends Fixture
     private $faker;
     private $passwordEncoder;
 
+    private const USERS = [
+        [
+            'email' => 'john@domain.com',
+            'fullname' => 'John Snow',
+            'password' => 'Welkom01!'
+        ]
+    ];
+
     public function __construct(UserPasswordEncoderInterface $passwordEncoder)
     {
         $this->faker = Faker::create();
@@ -21,27 +29,52 @@ class AppFixtures extends Fixture
 
     public function load(ObjectManager $manager)
     {
-        $this->loadUsers( $manager );
-        $this->loadPrintOrders( $manager );
+        $users = $this->getDummyUsersData();
+
+        $this->loadUsers( $manager, $users );
+        $this->loadPrintOrders( $manager, $users );
     }
 
-    private function loadUsers(ObjectManager $manager)
+    private function loadUsers(ObjectManager $manager, $users)
     {
-        $user = new User();
+        foreach( $users as $user_data ) {
+            $user = new User();
 
-        $user->setEmail('john@domain.com');
-        $user->setFullName('John Snow');
-        $user->setPassword(
-            $this->passwordEncoder->encodePassword( $user, 'John01!' )
-        );
+            $user->setEmail( $user_data['email'] );
+            $user->setFullName( $user_data['fullname'] );
+            $user->setPassword(
+                $this->passwordEncoder->encodePassword( $user, $user_data['password'] )
+            );
 
-        $this->addReference('user-john', $user);
+            $this->addReference( $this->convertToReferenceName( $user_data['fullname'] ), $user);
 
-        $manager->persist( $user );
+            $manager->persist( $user );
+        }
+
         $manager->flush();
     }
 
-    private function loadPrintOrders(ObjectManager $manager)
+    private function getDummyUsersData()
+    {
+        $users = [];
+
+        for( $i = 0; $i < 10; $i++ )
+        {
+            $users[] = [
+                'email' => $this->faker->email,
+                'fullname' => $this->faker->name,
+                'password' => 'Welkom01!'
+            ];
+        }
+
+        return array_merge( self::USERS, $users );
+    }
+
+    private function convertToReferenceName( $name ) {
+        return preg_replace('/\s+|\.+/', '-', strtolower( $name ) );
+    }
+
+    private function loadPrintOrders(ObjectManager $manager, $users)
     {
         for( $i = 0; $i < 100; $i++ )
         {
@@ -53,7 +86,7 @@ class AppFixtures extends Fixture
             $order->setWidth( rand(50, 500) );
             $order->setHeight( rand(50, 500) );
             $order->setCreateDate( $this->faker->dateTimeBetween('-2 years') );
-            $order->setUser( $this->getReference('user-john') );
+            $order->setUser( $this->getReference( $this->convertToReferenceName( $users[ rand( 0, count($users) - 1 ) ]['fullname'] ) ) );
 
             $manager->persist($order);
         }
