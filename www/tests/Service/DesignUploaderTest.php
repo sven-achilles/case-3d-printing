c0fdd976390095c0730e3c7a47573b469201d4c4
Sven van Zoelen <sven@achillescm.nl>
2018-08-01 11:08:19 +0200

    added simple unit test

---
+++ www/tests/Service/DesignUploaderTest.php
@@ -0,0 +1,24 @@
+<?php
+namespace App\Tests\Service;
+
+use App\Service\DesignUploader;
+use PHPUnit\Framework\TestCase;
+
+class DesignUploaderTest extends TestCase
+{
+    /**
+     * @var string $extension
+     *
+     * @testWith ["stl"]
+     *           ["obj"]
+     *           ["exe"]
+     *           ["xls"]
+     */
+    public function testUniqueFileNameGenerator( $extension )
+    {
+        $filename  = DesignUploader::getUniqueFileName( $extension );
+
+        $this->assertEquals( 32 + strlen( $extension ) + 1, strlen($filename) );
+        $this->assertTrue( !preg_match('/[A-Z]+[a-z]+[0-9]+\.' . $extension . '/', $filename) );
+    }
+}
